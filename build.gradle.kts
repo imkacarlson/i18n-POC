e2e4c046d9651d35ee610070ec5f657417129a58
Keith Carlson <imkacarlson@gmail.com>
2020-10-09 10:28:35 -0400

    adding polyglot npm dependency to gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     implementation("org.jetbrains:kotlin-react:16.13.1-pre.110-kotlin-1.4.10")
     implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.110-kotlin-1.4.10")
     implementation("org.jetbrains:kotlin-styled:1.0.0-pre.110-kotlin-1.4.10")
+
+    implementation(npm("node-polyglot", "2.4.0"))
 }
 kotlin {
     js {
